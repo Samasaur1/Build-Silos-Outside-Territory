a479a537f19a3592ba6c1a86fac3768a2c55fdd8
Sam <30577766+Samasaur1@users.noreply.github.com>
2019-02-17 21:38:32 -0500

    Add mod files

---
+++ silo.sql
@@ -0,0 +1,5 @@
+-- silo
+-- Author: Windows7
+-- DateCreated: 2/17/2019 1:11:30 PM
+--------------------------------------------------------------
+UPDATE Improvements SET CanBuildOutsideTerritory=1 WHERE Improvementype='IMPROVEMENT_MISSILE_SILO';
